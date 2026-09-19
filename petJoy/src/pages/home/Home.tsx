addf9d5c3b3f354e36eb0709b8de4407f657b11f
MariaAlbarado <mariaalbaradoandrade@outlook.com>
2026-09-19 09:36:27 -0300

    inicia projeto PetJoy

---
+++ petJoy/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>PetJoy</h1>
+      <p>Pets mais felizes, todos os dias!</p>
+    </div>
+  );
+}
+
+export default Home;
